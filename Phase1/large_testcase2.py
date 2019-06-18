68da50d3be74119bd0f41fdc6fa4a0ce2c81776c
Hiroto Shishido <s1250176@u-aizu.ac.jp>
2019-06-19 07:43:53 +0900

    final

--- Phase1/large_testcase2.py
+++ Phase1/large_testcase2.py
@@ -4,5 +4,5 @@ print("200 100 0 0");
 for i in range(0,200):
     print("%d %d" % (random.randint(0,1000),random.randint(0,1000)));
           
-for j in range(0,100):
+for i in range(0,100):
     print("%d %d" % (random.randint(1,200),random.randint(1,200)));
