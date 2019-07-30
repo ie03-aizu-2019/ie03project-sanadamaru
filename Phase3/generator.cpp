70d21ab7568153c42bb3578943c5836f4a619817
陽田和丈 <youdakazutomo@wlan-napt-001.u-aizu.ac.jp>
2019-07-31 07:56:15 +0900

    generator

---
+++ Phase3/generator.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using Pii = pair<int, int>;
+
+int main() {
+  int N = 10, M = 10, P = 10, Q = 10;
+  int MAX_xy = 100000;
+
+  cout << N << " " << M << " " << P << " " << Q << endl;
+  
+  random_device rnd;
+  {
+    map<Pii, bool> used;  
+    for ( int i = 0; i < N; i++ ) {
+      int x = rnd()%(MAX_xy+1);
+      int y = rnd()%(MAX_xy+1);
+      if ( used.count(Pii(x, y)) ) {
+	i--;
+	continue;
+      }
+      cout << x << " " << y << endl;
+      used[Pii(x, y)] = true;
+    }
+    
+    used.clear();  
+    for ( int i = 0; i < M; i++ ) {
+      int x = rnd()%(N)+1;
+      int y = rnd()%(N)+1;
+      if ( x > y ) swap(x, y);    
+      if ( used.count(Pii(x, y)) || x == y ) {
+	i--;
+	continue;
+      }
+      cout << x << " " << y << endl;
+      used[Pii(x, y)] = true;
+    }
+
+    used.clear();
+    for ( int i = 0; i < P; i++ ) {
+      int x = rnd()%(MAX_xy+1);
+      int y = rnd()%(MAX_xy+1);
+      if ( used.count(Pii(x, y)) ) {
+	i--;
+	continue;
+      }
+      cout << x << " " << y << endl;
+      used[Pii(x, y)] = true;
+    }
+
+    used.clear();
+    for ( int i = 0; i < Q; i++ ) {
+      int x = rnd()%(N)+1;
+      int y = rnd()%(N)+1;
+      if ( rnd()%2 == 0 ) x += N;
+      if ( rnd()%2 == 0 ) y += N;      
+      if ( used.count(Pii(x, y)) ) {
+	i--;
+	continue;
+      }
+      if ( x > N ) cout << 'C' << x-N << " ";
+      else cout << x << " ";
+      if ( y > N ) cout << 'C' << y-N << " ";
+      else cout << y << " ";
+      cout << rnd()%10+1 << endl;      
+      used[Pii(x, y)] = true;
+    }
+  }
+  
+  return 0;
+}
